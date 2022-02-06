22a092f27a6909ac0bcfbaacd73c2fecb7891cdc
Hector Martinez cervera <hmartine@cbr9s3.42barcelona.com>
2022-02-06 17:33:40 +0100

    delete a.out

---
+++ Funciones_de_libc/ft_calloc.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmartine <hmartine@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/06 14:25:20 by hmartine          #+#    #+#             */
+/*   Updated: 2022/02/06 16:29:47 by hmartine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+
+void*	ft_calloc(size_t count, size_t size);
+
+void*	ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*ptr;
+	
+	ptr = (void*) malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	while (size > 0)
+	{
+		ptr[size] = ('\0');
+		size--;
+	}
+	return ((void*) ptr);
+}
+
+int	main(void)
+{
+	char*	str;
+	int		i;
+
+	printf("Original\n");
+	i = 0;
+	str = calloc(2, sizeof(char));
+	while (i < 3)
+	{
+		if (str[i] != '\0')
+			printf("No es nulo\n");
+		else
+			printf("Es nulo\n");
+		i++;
+	}
+	printf("Creada\n");
+	i = 0;
+	str = ft_calloc(2, sizeof(char));
+	while (i < 3)
+	{
+		if (str[i] != '\0')
+			printf("No es nulo\n");
+		else
+			printf("Es nulo\n");
+		i++;
+	}
+}
