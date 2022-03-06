465dbc622a841695bdc33cf3b61fdff0eba27eb5
Hector Martinez cervera <hmartine@cbr11s6.42barcelona.com>
2022-03-06 18:41:02 +0100

    bonus

--- Parte_bonus/ft_lstmap.c
+++ Parte_bonus/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: hmartine <hmartine@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 18:15:10 by hmartine          #+#    #+#             */
-/*   Updated: 2022/03/06 01:02:05 by hmartine         ###   ########.fr       */
+/*   Updated: 2022/03/06 18:00:47 by hmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*first;
 	t_list	*new;
-	t_list	*ini;
 
 	if (!f || !del)
 		return (NULL);
-	ini = NULL;
+	first = NULL;
 	while (lst)
 	{
-		new = (t_list *)malloc(sizeof(t_list));
+		new = ft_lstnew(f(lst->content));
 		if (!new)
 			return (NULL);
-		new->content = f(lst->content);
-		if (del)
-		{
-			del(new->content);
-			free(new);
-		}
+		ft_lstadd_back(&first, new);
 		lst = lst->next;
-		new = new->next;
 	}
-	new = ini;
-	return (new);
+	return (first);
 }
